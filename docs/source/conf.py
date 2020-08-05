41cd7dbec66a698240db0c8718a57741adbcd2d1
MAZY Francois-Remi <francois-remi.mazy@inria.fr>
2020-08-05 09:14:13 +0200

    20200805 0914

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,9 +34,9 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.mathjax',
     'sphinx.ext.napoleon',
-    'sphinxcontrib.bibtex',
     'nbsphinx',
     'sphinx.ext.autosectionlabel']
+    # 'sphinxcontrib.bibtex',
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
