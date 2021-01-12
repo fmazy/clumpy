f476be91a77a30295a4ac170a47224a78305dbc3
MAZY Francois-Remi <francois-remi.mazy@inria.fr>
2021-01-12 20:59:36 +0100

    20210112 2059

--- clumpy/resampling/under_sampling.py
+++ clumpy/resampling/under_sampling.py
@@ -2,6 +2,30 @@
 
 import numpy as np
 
+def create_sampling_strategy_function(gamma=1, beta=None):
+    def compute_sampling_strategy(y, u=None):
+        e_, n = np.unique(y, return_counts=True)
+            
+        if u is None:
+            # on part du principe que u est le majoritaire.
+            id_u_ = list(n).index(n.max())
+            u = e_[id_u_]
+        else:
+            id_u_ = list(e_).index(u)
+        
+        sampling_strategy = {}
+        for i, e_i in enumerate(e_):
+            if e_i != u:
+                # si e_i est différent de u, on prend tous les éléments.
+                # (nécessaire pour appliquer la formule !)
+                sampling_strategy[e_i] = n[i]
+            else:
+                sampling_strategy[e_i] =  int(n[e_!=u].max() * gamma)
+        
+        return(sampling_strategy)
+    
+    return(compute_sampling_strategy)
+
 def compute_sampling_strategy(y, gamma=1, beta=None, u=None, return_beta=False):
     """ Compute sampling strategy
     
