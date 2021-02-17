f63ef756b7c5b91f61ed50f277de8851b676f706
MAZY Francois-Remi <francois-remi.mazy@inria.fr>
2021-02-17 15:50:07 +0100

    20210217 1550

--- clumpy/metrics/__init__.py
+++ clumpy/metrics/__init__.py
@@ -2,4 +2,3 @@
 
 from ._log_score import log_score, compute_a, log_scorer, under_sampling_log_scorer
 from ._weighted_kolmogorov_smirnov import weighted_kolmogorov_smirnov
-from ._mcdf import calcul
