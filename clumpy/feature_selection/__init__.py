2bca0beeda764ae22b0cd8352c5a734702ce7319
fmazy <francois-remi.mazy@inria.fr>
2022-04-07 10:56:44 +0200

    20220407 1056

--- clumpy/feature_selection/__init__.py
+++ clumpy/feature_selection/__init__.py
@@ -1,5 +1,6 @@
 from ._variance_threshold import VarianceThreshold
 from ._correlation_threshold import CorrelationThreshold
+from ._mRMR import MRMR
 
 feature_selectors = {'variance_threshold':VarianceThreshold,
                      'correlation_threshold':CorrelationThreshold}
--- clumpy/feature_selection/_mRMR.py
+++ clumpy/feature_selection/_mRMR.py
@@ -11,8 +11,10 @@ class MRMR(FeatureSelector):
         
         F = []
         for j in range(X.shape[1]):
-            F.append(np.corrcoef(np.vstack((X[:,j], y)))[0,1])
+            F.append(np.abs(np.corrcoef(np.vstack((X[:,j], y)))[0,1]))
         F = np.abs(F)
+        
+        corr_X = np.abs(np.corrcoef(X))
         
         T_bar = np.arange(X.shape[1])
         
@@ -21,4 +23,14 @@ class MRMR(FeatureSelector):
         T_bar = np.delete(T_bar, id_max)
         T = np.array([id_max])
         
-        return(np.arange(X.shape[1]))
+        for j in range(self.s):
+            
+            alpha = F[T_bar] / np.sum(corr_X[T_bar, :][:, T], axis=1)
+            
+            b = np.argmax(alpha)
+            T_bar = np.delete(T_bar, b)
+            T = np.append(T, b)
+        
+        self._cols_support = list(T)
+        
+        return(self)
