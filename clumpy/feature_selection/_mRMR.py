267c7d3d9f52ee3597f8c82b5f07deb17b6f2da6
fmazy <francois-remi.mazy@inria.fr>
2022-04-07 10:27:42 +0200

    20220407 1027

--- clumpy/feature_selection/_mRMR.py
+++ clumpy/feature_selection/_mRMR.py
@@ -4,8 +4,8 @@ import numpy as np
 from ._feature_selector import FeatureSelector
 
 class MRMR(FeatureSelector):
-    def __init__(self, k):
-        self.k = k
+    def __init__(self, s):
+        self.s = s
     
     def fit(self, X, y):
         
@@ -14,6 +14,11 @@ class MRMR(FeatureSelector):
             F.append(np.corrcoef(np.vstack((X[:,j], y)))[0,1])
         F = np.abs(F)
         
+        T_bar = np.arange(X.shape[1])
         
+        id_max = np.argmax(F)
+        
+        T_bar = np.delete(T_bar, id_max)
+        T = np.array([id_max])
         
         return(np.arange(X.shape[1]))
